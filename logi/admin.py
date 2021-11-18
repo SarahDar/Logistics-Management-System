e24590f7d52f26eca395683271fc701dbe44437e
Now33d <23100042@lums.edu.pk>
2021-11-18 21:49:43 +0500

    done making tables

--- logi/admin.py
+++ logi/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SellerProduct
+from .models import ClientProduct
+from .models import Login
+
+from .models import Seller
+from .models import Client
+from .models import Warehouse
+from .models import Product
+from .models import Rider
+    
+admin.site.register(SellerProduct)
+admin.site.register(ClientProduct)
+admin.site.register(Login)
+
+admin.site.register(Seller)
+admin.site.register(Client)
+admin.site.register(Warehouse)
+admin.site.register(Rider)
+admin.site.register(Product) 
