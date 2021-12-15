7086f24cf6babe19d672fca54cd962ecb93de62d
faaiqbilal <faaiqbilal@outlook.com>
2021-12-16 02:02:20 +0500

    more updates

--- seller/urls.py
+++ seller/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.login),
     path('products', views.seller_track_pack),
-    path('client', views.seller_get_clientinfo),
+    path('client', views.seller_client_info),
     path('updates', views.seller_check_updates)
 ]
