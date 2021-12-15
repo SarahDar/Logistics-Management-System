926451d9f585963d0a7726c33e5c912128fc0e2a
faaiqbilal <faaiqbilal@outlook.com>
2021-12-15 21:44:52 +0500

    update

--- seller/urls.py
+++ seller/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login),
+    path('home', views.home),
     path('products', views.seller_track_pack),
     path('client', views.seller_get_clientinfo),
     path('updates', views.seller_check_updates)
--- seller/views.py
+++ seller/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.db import connection
+from django.contrib import messages
 
 # Create your views here.
 def dictfetchall(cursor): 
@@ -12,16 +13,28 @@ def dictfetchall(cursor):
 def home(request):
     return render(request, 'SellerMenu.html')
 
+def home_wargs(request, username):
+    # for testing purposes, we are returning obtain seller related product information
+    with connection.cursor() as cursor:
+        query = "SELECT * FROM Seller WHERE sellerID=\"%s\";" % username
+        cursor.execute(query)
+        rows = dictfetchall(cursor)
+
+    return render(request, 'SellerTrackPackages.html', {'data': rows})
+
+
 def login(request):
     if request.method=='POST':
         username = request.POST['username']
         password = request.POST['password']
-    
+
         logged_in = authenticate(username, password)
         if logged_in:
-            return redirect("home")
+            print("authentication was successful")
+            return redirect('/seller/home')
         else:
             messages.info(request, "invalid credentials")
+            return redirect('/')
 
     else:
         return render(request, 'SellerLogin.html')
@@ -36,14 +49,20 @@ def seller_check_updates(request):
     return render(request, 'SellerCheckUpdates.html')
 
 def authenticate(username, password):
+    if username == "":
+        return False
+
     with connection.cursor() as cursor:
-        query = "SELECT userPassword WHERE userName=%s" % username
+        query = "SELECT userPassword FROM LoginInfo WHERE LoginInfo.ID=\"%s\";" % username
         cursor.execute(query)
         rows = dictfetchall(cursor)
     
-    print(rows)
-    row = rows[0]
-    if row["password"] == password:
-        return True
+    if rows is not None:
+        print(rows)
+        row = rows[0]
+        if row["userPassword"] == password:
+            return True
+        else:
+            return False
     else:
         return False
